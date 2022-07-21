10e5857235a9cffb266b806fff5a54f244b2b761
Melin.Chao <melin.chao@weblink.com.tw>
2022-07-21 08:15:52 +0800

    add class

---
+++ SpringIocAnnotation/src/main/java/com/melin/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.melin.controller; /**
+  * @Author Melin Chao
+  * @Since 2022/7/21
+  */public class UserController {
+}
---
+++ SpringIocAnnotation/src/main/java/com/melin/service/UserService.java
@@ -0,0 +1,5 @@
+package com.melin.service; /**
+  * @Author Melin Chao
+  * @Since 2022/7/21
+  */public interface UserService {
+}
