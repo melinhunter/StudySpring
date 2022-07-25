2a7647593036337dc657eeef3f4ca2b99b1fc6e2
DESKTOP-25G9A70\melin <melinhunter@gmail.com>
2022-07-25 22:10:56 +0800

    study

---
+++ springIocJavaconfig/src/main/java/com/melin/SecondJavaConfig.java
@@ -0,0 +1,11 @@
+package com.melin;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author melin
+ * @since 2022/7/25
+ */
+@ComponentScan
+public class SecondJavaConfig {
+}
